e7dde7d5a7a85232cc22ad988186ac85a7ff9f24
Segun <victor.segunigebello@gmail.com>
2025-12-19 07:37:58 +0100

    updated layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@
 import "./globals.css";
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import { ClerkProvider } from '@clerk/nextjs'
 
 // const inter = Inter({ subsets: ["latin"] });
